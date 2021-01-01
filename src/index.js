68d5060961b481ab3a8bb78319edb0084d3391c9
Mehrab <mehrab.zaman@outlook.com>
2021-01-01 17:12:32 +0600

    Added CORS

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const booksRouter = require('./routers/books')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT
 
+app.use(cors())
 app.use(express.json())
 app.use(userRouter)
 app.use(booksRouter)
